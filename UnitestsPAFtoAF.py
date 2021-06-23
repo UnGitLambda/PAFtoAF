dd6b8e1375851d4dd77e0ef0fcfca684ba2c049e
UnHommeLambda <ec.eyalcohen@gmail.com>
2021-06-23 19:24:07 +0200

    Update UnitestsPAFtoAF.py

--- UnitestsPAFtoAF.py
+++ UnitestsPAFtoAF.py
@@ -11,18 +11,29 @@ from copy import deepcopy
 import random as r
 import os
 
-nodes = 3
-edges = 0
-preferences = 0
+if os.path.exists("temp_unitest_PAFtoAF"):
+    if os.path.isdir("temp_unitest_PAFtoAF"):
+        dir_name = "temp_unitest_PAFtoAF"
+        create = False
+    else:
+        dir_name = "temp_unitest_PAFtoAF_dir"
+        create = True
+else:
+    dir_name = "temp_unitest_PAFtoAF"
+    create = True
+
 count = 0
 actual_PAF = (paf.args, paf.attacksFrom, paf.preferences)
 error = None
+suppress = False
+file = ''
 
-def generate_PAF():
-    global nodes
-    global edges
-    global preferences
+def generate_PAF(nodes, function = print):
     global error
+    global count
+    global file
+    edges = 0
+    preferences = 0
     attsFrom = dict()
     prefs = dict()
     arg_gen = set([arg for arg in range(nodes+1)])
@@ -36,45 +47,74 @@ def generate_PAF():
             edge_list.append((arg1,arg2))
     pref_list = edge_list.copy()
     for i in range(2**nodes + 1):
+        new_file()
         lim = 0
         if r.random()>0.25:
-            att = r.choice(edge_list)
-            edge_list.remove(att)
-            attsFrom[att[0]].add(att[1])
-            lim = 0.55
-            edges += 1
+            try:
+                att = r.choice(edge_list)
+                edge_list.remove(att)
+                attsFrom[att[0]].add(att[1])
+                function(file, att, "attack")
+                lim = 0.55
+                edges += 1
+            except(IndexError):
+                continue
         if r.random()>lim:
-            pref = r.choice(pref_list)
-            pref_list.remove(pref)
-            prefs[pref[0]].add(pref[1])
-            if pref[0] in prefs[pref[1]] or pref[0] == pref[1]:
-                error = paf.PreferenceException
-            preferences += 1
+            try:
+                pref = r.choice(pref_list)
+                pref_list.remove(pref)
+                prefs[pref[0]].add(pref[1])
+                if pref[0] in prefs[pref[1]] or pref[0] == pref[1]:
+                    error = paf.PreferenceException
+                    fileformat = ("ptgf", "papx")[file.endswith("papx") or file.endswith("apx")]
+                    errfile = toFILE((arg_gen, attsFrom, prefs), fileformat, "pref_error")
+                else:
+                    function(file, pref, "preference")
+                preferences += 1
+            except(IndexError):
+                continue
         yield((arg_gen, attsFrom, prefs))
         if error is paf.PreferenceException:
+            global suppress
             prefs[pref[0]].remove(pref[1])
+            if suppress:
+                os.remove(errfile)
             error = None
 
-def toFILE(PAF, fileformat):
-    if fileformat == "ptgf":
-        toPTGF(PAF)
-    elif fileformat == "papx":
-        toPAPX(PAF)
-    else: raise paf.FormatException("Wrong Format while testing")
-
-def toPTGF(PAF):
+def new_file(): #TODO ?????
+    global file
     global count
-    with open("test{count}.ptgf", "w+") as file:
+    new_file = file.replace(str(count-1), str(count))
+    with open(file, "r") as openfile:
+        content = openfile.read()
+    with open(new_file, "w+") as openfile:
+        openfile.write(content)
+    #os.rename(file, new_file)
+    file = new_file
+
+def toFILE(PAF, fileformat, offset = None):
+    if fileformat == "ptgf":
+        file = toPTGF(PAF, offset)
+    elif fileformat == "papx":
+        file = toPAPX(PAF, offset)
+    else: raise paf.FormatException("Wrong Format while testing. Please use ptgf or papx format.")
+    return(file)
+
+def toPTGF(PAF, offset):
+    global count
+    file = "test{}-{}.ptgf".format(count, ("","".join(["",str(offset)]))[offset is not None])
+    with open(file, "w+") as openfile:
         for arg in PAF[0]:
-            file.write(f'{arg}\n')
-        file.write("#\n")
+            openfile.write(f'{arg}\n')
+        openfile.write("#\n")
         for arg1 in  PAF[1].keys():
             for arg2 in PAF[1][arg1]:
-                file.write(f"{arg1} {arg2}\n")
-        file.write("#\n")
+                openfile.write(f"{arg1} {arg2}\n")
+        openfile.write("#\n")
         for arg1 in PAF[2].keys():
             for arg2 in PAF[2][arg1]:
-                file.write(f"{arg1} {arg2}\n")
+                openfile.write(f"{arg1} {arg2}\n")
+    return(file)
                 
 def addPTGF(file, relation, name):
     if name == "argument":
@@ -83,32 +123,33 @@ def addPTGF(file, relation, name):
             openfile.seek(0,0)
             openfile.write(f"{relation}\n{content}")
     elif name == "attack":
-# ============================================================================= #TODO
-#         with open(file, "r+") as openfile:
-#             content = openfile.read()
-#             att_index = content.index("#")
-#             openfile.seek(att_index+2, 0)
-#             content = content[att_index+2::]
-#             openfile.write(f"{relation[0]} {relation[1]}")
-#             openfile.write(content)
-# =============================================================================
-        print("todo")
-    elif name == "pref":
-        with open(file, "a") as openfile:
-            openfile.write(f"{relation[0]} {relation[1]}")
+        with open(file, "r+") as openfile:
+            openfile.seek(0,0)
+            content = openfile.read()
+            openfile.seek(0,0)
+            att_index = content.rindex("#")
+            beginning_content = content[:att_index:]
+            end_content = content[att_index::]
+            openfile.write(f"{beginning_content}{relation[0]} {relation[1]}\n{end_content}")
+    elif name == "preference":
+        with open(file, "a+") as openfile:
+            openfile.write(f"{relation[0]} {relation[1]}\n")
+    else: raise paf.FormatSyntaxException("Format Syntax Error while testing.")
         
 
-def toPAPX(PAF):
+def toPAPX(PAF, offset):
     global count
-    with open(f"test{count}.papx", "w+") as file:
+    file = "test{}{}.papx".format(count, ("","".join(["",str(offset)]))[offset is not None])
+    with open(file, "w+") as openfile:
         for arg in PAF[0]:
-            file.write(f'arg({arg})\n')
+            openfile.write(f'arg({arg})\n')
         for arg1 in  PAF[1].keys():
             for arg2 in PAF[1][arg1]:
-                file.write(f"att({arg1},{arg2})\n")
+                openfile.write(f"att({arg1},{arg2})\n")
         for arg1 in PAF[2].keys():
             for arg2 in PAF[2][arg1]:
-                file.write(f"pref({arg1},{arg2})\n")
+                openfile.write(f"pref({arg1},{arg2})\n")
+    return(file)
 
 def addPAPX(file, relation, name):
     if name == "argument":
@@ -120,29 +161,42 @@ def addPAPX(file, relation, name):
         with open(file, "a") as openfile:
             if name == "attack":
                 openfile.write(f"att({relation[0]},{relation[1]})")
-            elif name == "pref":
+            elif name == "preference":
                 openfile.write(f"pref({relation[0]},{relation[1]})")
             else: raise paf.FormatSyntaxException("Format Syntax Error while testing.")
 
-def test(timeout):
+def test(timeout, start_nodes = 0, fileformat = "ptgf"):
     global count
+    global file
+    nodes = start_nodes
     start = time.time()
+    file = toFILE(([arg for arg in range(nodes+1)], dict(), dict()), fileformat)
+    if fileformat == "ptgf":
+        function = addPTGF
+    elif fileformat == "papx":
+        function = addPAPX
+    else: raise paf.FormatException("Format Exception while testing. Please use ptgf or papx as file format.")
     while (time.time()-start)<=timeout:
-        count += 1
-        for actual_PAF in generate_PAF():
-            print(actual_PAF[0])
-            print(actual_PAF[1])
-            print(actual_PAF[2])
+        for actual_PAF in generate_PAF(nodes, function = function):
+            print(actual_PAF)
             print(error)
+            count += 1
+            if(time.time()-start>timeout):break
+        nodes += 1
+        function(file, nodes, "argument")
+    print(count)
 
+if create:
+    os.mkdir(dir_name)
 
-os.mkdir("temp_unitest_PAFtoAF")
+os.chdir(dir_name)
 
-os.chdir("temp_unitest_PAFtoAF")
+test(1)
+time.sleep(60)
 
 os.chdir("..")
 
-for file in os.scandir("temp_unitest_PAFtoAF"):
+for file in os.scandir(dir_name):
     os.remove(file)
     
-os.rmdir("temp_unitest_PAFtoAF")
+os.rmdir(dir_name)
